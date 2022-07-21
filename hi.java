3742964c529dc191ea505e8651e5f078b14e1234
Fred Xu <Fred Xu>
2022-07-21 08:57:49 -0400

    Testing add

--- hi.java
+++ hi.java
@@ -1,5 +1,7 @@
 class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Fred's edit, testing vscode push");
+        System.out.println("Test #2");
     }
 }
