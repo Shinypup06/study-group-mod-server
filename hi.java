f9e000e347a96a274514f48c5194762260a056b6
Fred Xu <Fred Xu>
2022-07-22 09:32:11 -0400

    update

--- hi.java
+++
@@ -1,7 +0,0 @@
-class TestApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Fred's edit, testing vscode push");
-        System.out.println("Test #2");
-    }
-}
