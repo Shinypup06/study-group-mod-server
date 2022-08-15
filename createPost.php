be7620ba94a414a8330de6fb2542489aff583f28
Fred Xu <Fred Xu>
2022-08-15 09:04:18 -0400

    Php database connection

--- createPost.php
+++ createPost.php
@@ -1,4 +1,5 @@
 <?php
+    //variables for the posting page
     $title = $_POST['title'];
     $file = $_POST['file'];
     $text = $_POST['text'];
@@ -6,5 +7,7 @@
     //Database Connection
     //blaabasblsafjdgoihoqwhtoqhwgepogwo
     
-    
+    $conn = new mysqli('localhost', 'root', '//set this up');
+    if ($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error)
 ?>
