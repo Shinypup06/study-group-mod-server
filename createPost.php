4854634cc2e92c913f0a4c64a18fc6f2caef23a1
Fred Xu <Fred Xu>
2022-08-16 10:19:30 -0400

    edit

--- createPost.php
+++ createPost.php
@@ -2,15 +2,3 @@
 
 print_r($_POST);
     
-//variables for the posting page
-    /*$title = $_POST['title'];
-    $file = $_POST['file'];
-    $text = $_POST['text'];
-
-    //Database Connection
-    //blaabasblsafjdgoihoqwhtoqhwgepogwo
-    
-    $conn = new mysqli('localhost', 'root', '//set this up');
-    if ($conn->connect_error){
-        die('Connection Failed : '.$conn->connect_error)*/
-    
